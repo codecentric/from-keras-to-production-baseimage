54388cef818900ab4a37f621ac12d593d617e4a9
Mark Keinhörster <mark.keinhoerster@googlemail.com>
2018-09-10 21:15:49 +0200

    Update jupyter_notebook_config.py

--- configs/jupyter_notebook_config.py
+++ configs/jupyter_notebook_config.py
@@ -211,7 +211,7 @@ c.NotebookApp.ip = "0.0.0.0"
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-c.NotebookApp.notebook_dir = u'/data2day2018'
+c.NotebookApp.notebook_dir = u'/keras2production'
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
